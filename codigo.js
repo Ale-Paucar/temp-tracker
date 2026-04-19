e36fdaccf0b285bd26ac3bf2cde25aa98d8a56a2
Ale Paucar <nuala.ni.ale@gmail.com>
2026-04-19 00:46:38 -0500

    primer commit

---
+++ codigo.js
@@ -0,0 +1,26 @@
+const claveApi = "e51a25670bce4e6e95f15153261904";
+const idioma = "es";
+const inpCiudad = document.getElementById("input-ciudad");
+
+async function obtenerClima() {
+    const ciudad = inpCiudad.value
+
+    if(!ciudad){
+        alert("¡Por favor, ingresa una ciudad!")
+        return;
+    }
+    const apiClimaActual = `https://api.weatherapi.com/v1/current.json?q=${ciudad}&lang=${idioma}&key=${claveApi}`
+    const res = await fetch(apiClimaActual);
+    let data = await res.json();
+    mostrarClima(data)
+}
+
+
+function mostrarClima(data) {
+    document.querySelector(".clima-icono").src = data.current.condition.icon ;
+    document.querySelector(".clima-texto").innerHTML = data.current.condition.text;
+    document.querySelector(".temp").innerHTML = data.current.temp_c + "°C";
+    document.querySelector(".ciudad").innerHTML = data.location.name;
+    document.querySelector(".humedad" ).innerHTML = data.current.humidity + '%';
+    document.querySelector(".viento").innerHTML = data.current.wind_kph + "km/h";
+}
